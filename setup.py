0d8389f8476ebb86d874dc559d460bacdc58f188
Sean Guan <zhxiang.guan@gmail.com>
2018-02-02 08:42:26 +0800

    create pip setup files.

---
+++ setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+
+setup(
+    name='shop',
+    packages=['shop'],
+    include_package_data=True,
+    install_requires=[
+        'flask',
+        'Flask-Alchemy',
+        'Flask-Migrate',
+        'psycopg2',
+    ],
+    setup_requires=[
+        'pytest-runner',
+    ],
+    tests_require=[
+        'pytest',
+    ],
+)
