9e03499d29aa6aefa408290d5986eb4961aaa8b6
Sean Guan <zhxiang.guan@gmail.com>
2018-02-02 14:44:10 +0800

    Tests: create folder and smoke test.

---
+++ tests/shop_tests.py
@@ -0,0 +1,11 @@
+import os
+import unittest
+
+import shop
+
+class ShopTestCase(unittest.TestCase):
+    def somkeTest(self):
+        assert 1 + 1 == 2
+
+if __name__ == '__main__':
+    unittest.main()
