9462080daa9cc31edfccd1ec31c3ce68b8e297d7
Sean Guan <zhxiang.guan@gmail.com>
2018-02-04 23:45:08 +0800

    Template: create index and layout template.

--- shop/index.py
+++ shop/index.py
@@ -1,9 +1,7 @@
+from flask import  render_template
 from shop import app
 
 @app.route('/')
-def hello_world():
-    return '<Html><h1>home page</h1></Html>'
-
-@app.route('/hw')
-def hw():
-    return 'Hello, world'
+@app.route('/index')
+def index():
+    return render_template('index.html')
