1de1ae15077efe2e408e6c2343fcfa1ed4e27d85
Sean Guan <zhxiang.guan@gmail.com>
2018-02-09 10:19:57 +0800

    Test: using global fixture way to implement tests.

--- tests/api/test_products_apis.py
+++ tests/api/test_products_apis.py
@@ -6,20 +6,17 @@ from shop import app
 TEST_TEXT = 'test'
 TEST_ID = 1
 
-
-app = app.test_client()
-
-def test_echo():
+def test_echo(app):
     reponse = app.get('api/echo/{}'.format(TEST_TEXT), follow_redirects=True)
     
     assert TEST_TEXT in reponse.get_data(as_text=True), 'Test Echo'
 
-def test_get_products():
+def test_get_products(app):
     reponse = app.get('api/products', follow_redirects=True)
 
     assert reponse.get_data(as_text=True) == '[]\n', 'Test Products.'
 
-def test_get_product():
+def test_get_product(app):
     reponse = app.get('api/products/{}'.format(TEST_ID), follow_redirects=True)
 
     assert reponse.get_data(as_text=True) == 'null\n',  'Test get Product.'
