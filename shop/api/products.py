62fbe7d32cd30401686571c89d634e6bd147a41e
Sean Guan <zhxiang.guan@gmail.com>
2018-02-10 23:01:46 +0800

    Product API: implement it, using helper.

--- shop/api/products.py
+++ shop/api/products.py
@@ -1,38 +1,50 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from flask import request, Blueprint, jsonify
+from http import HTTPStatus
+from flask import request, jsonify
 
-from shop import db
-from shop.model.LanguageProduct import LanguageProduct
-
+from . import api_orm_helper as helper
 from . import api
 
-@api.route('/echo/<string:argument>', methods=['GET', 'POST'])
-def echo(argument):
-    """ basic api argument test, for check api is work"""
-    return argument
+from shop import db
+from shop.model.Product import Product
 
-@api.route('/products', methods=['GET'])
+def exact_request(request, product=None):
+    type_id = request.form['type_id']
+    manufacturing = request.form['manufacturing']
+
+    if not product:
+        return Product(type_id, manufacturing)
+
+    product.type_id = type_id | product.type_id
+    product.manufacturing = manufacturing | product.manufacturing
+
+    return product
+
+@api.route('/products', methods=['GET', 'POST'])
 def get_list_of_products():
     page = request.args.get('page', default=1, type=int)
     limited = request.args.get('limited', default=1, type=int)
-    lng = request.args.get('lng', default=0, type=int)
 
-    products = db.session.query(LanguageProduct)\
-        .filter(LanguageProduct.language_id == lng)\
-        .all()
+    if request.method == 'POST':
+        return helper.append(exact_request(request))
 
-    return jsonify(products)
+    return helper.select_all(Product)
 
 @api.route('/products/<id>', methods=['GET', 'PUT', 'DELETE'])
 def get_product(id):
-    lng = request.args.get('lng', default=0, type=int)
-    product = db.session.query(LanguageProduct)\
-        .filter(LanguageProduct.product_id == id)\
-        .filter(LanguageProduct.language_id == lng)\
-        .one_or_none()
+    product = helper.select_by_id(Product, id)
 
-    result = product if product else None
+    if not product:
+        return helper.not_found()
 
-    return jsonify(result)
+    if request.method == 'GET':
+        return jsonify(product.serialize()), HTTPStatus.OK
+    elif request.method == 'DELETE':
+        return helper.delete(product)
+    elif request.method == 'PUT':
+        product = exact_request(request, product)
+        return helper.update(product)
+
+    return helper.bad_request()
