28bd0dceb11f1559bb064ef6618f5850b1488cc1
Sean Guan <zhxiang.guan@gmail.com>
2018-02-11 23:35:45 +0800

    Api orm helper: add examine error decoeator to handling query raise error.

--- shop/api/api_orm_helper.py
+++ shop/api/api_orm_helper.py
@@ -6,25 +6,40 @@ from flask import jsonify
 
 from shop import db
 
+def examime_error(func):
+    """ when query fail, return bad request"""
+    def with_try(*args, **kwargs):
+        try:
+            return func(*args, **kwargs)
+        except:
+            db.session.rollback()
+            return bad_request()
+    return with_try
+
+@examime_error
 def append(model):
     db.session.add(model)
     db.session.commit()
 
     return jsonify(model.serialize()), HTTPStatus.CREATED
 
+@examime_error
 def delete(model):
     db.session.delete(model)
     db.session.commit()
+
     return jsonify(model.serialize()), HTTPStatus.NO_CONTENT
 
+@examime_error
 def update(model):
     db.session.commit()
     return jsonify(model.serialize()), HTTPStatus.CREATED
 
-def select_by_id(cls, id):
-    return db.session.query(cls)\
-            .filter(cls.id == id)\
-            .one_or_none()
+@examime_error
+def select_all(cls):
+    return jsonify([ model.serialize() for model in db.session.query(cls).all() ]),\
+            HTTPStatus.OK
+
 
 def not_found():
     return jsonify(), HTTPStatus.NOT_FOUND
@@ -32,6 +47,7 @@ def not_found():
 def bad_request():
     return jsonify(), HTTPStatus.BAD_REQUEST
 
-def select_all(cls):
-    return jsonify([ model.serialize() for model in db.session.query(cls).all() ]),\
-            HTTPStatus.OK
+def select_by_id(cls, id):
+    return db.session.query(cls)\
+            .filter(cls.id == id)\
+            .one_or_none()
