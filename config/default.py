34af6db4aa3cd850706e135d860a72fb3536290f
Sean Guan <zhxiang.guan@gmail.com>
2018-03-02 14:10:02 +0800

    Default config: update it.

--- config/default.py
+++ config/default.py
@@ -1,3 +1,12 @@
+import os
+
 TESTING = False
 DEBUG = False
 SQLALCHEMY_ECHO = False
+
+DB_URL = os.getenv('DB_URL')
+
+SQLALCHEMY_DATABASE_URI = \
+        'postgresql://{db_url}?client_encoding=urf8'.format(db_url=db_url)
+
+SECRET_KEY = os.getenv('SECRET_KEY')
