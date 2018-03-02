89239eb93677c7df790075ffaf70b184bf2bdbe5
Sean Guan <zhxiang.guan@gmail.com>
2018-03-02 14:26:44 +0800

    fix typo

--- config/default.py
+++ config/default.py
@@ -7,6 +7,6 @@ SQLALCHEMY_ECHO = False
 DB_URL = os.getenv('DB_URL')
 
 SQLALCHEMY_DATABASE_URI = \
-        'postgresql://{db_url}?client_encoding=urf8'.format(db_url=db_url)
+        'postgresql://{db_url}?client_encoding=urf8'.format(db_url=DB_URL)
 
 SECRET_KEY = os.getenv('SECRET_KEY')
