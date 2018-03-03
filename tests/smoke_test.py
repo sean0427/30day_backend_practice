2241f8eb06e07053d05e740533715c02e1cc1deb
Sean Guan <zhxiang.guan@gmail.com>
2018-03-03 21:06:14 +0800

    Rename shop_tests to smoke_test

--- tests/smoke_test.py
+++ tests/smoke_test.py
@@ -1,11 +1,6 @@
 import os
 import unittest
 
-import shop
-
 class ShopTestCase(unittest.TestCase):
     def somkeTest(self):
         assert 1 + 1 == 2
-
-if __name__ == '__main__':
-    unittest.main()
